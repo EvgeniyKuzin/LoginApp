2eaf5ae617d9a44434d1c77b9bb01dc05f4977cd
EvgeniyKuzin <vaglover@yandex.ru>
2023-02-13 02:21:54 +0300

    UI is ready

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Evgeniy Kuzin on 13.02.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+/*
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        self.view.layer.addSublayer(gradientLayer)
+*/
+        
+    }
+    
+
+
+}
