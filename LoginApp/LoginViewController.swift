6bc4f71f60c001621fbe7f357005ebe3c1e23b64
EvgeniyKuzin <vaglover@yandex.ru>
2023-02-13 11:48:30 +0300

    adding alert controllers for username and password

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        
+        logInButton.layer.cornerRadius = 10
     }
+    
+let userName = "User"
+let password = "1111"
+    
+    
+    @IBAction func forgotUserNameButtonTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your username is User")
+    }
+    
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is 1111")
+    }
+    
+    
 }
 
+extension LoginViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
